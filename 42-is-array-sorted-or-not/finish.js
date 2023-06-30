6d5d91913438aa6b5338eee5d73499e6aee9871e
krawnick <kraw.nick@gmail.com>
2023-06-30 20:07:04 +0500

    tasks 42-43 are ready

--- 42-is-array-sorted-or-not/finish.js
+++ 42-is-array-sorted-or-not/finish.js
@@ -16,6 +16,31 @@ const b = [4, 10, 14, 25, 25, 50]
 const c = [150, 132, 80, 40]
 const d = [15, 26, 10, 23, 85]
 
+// console.log(a.some((element) => typeof element === 'number'))
+
+const arraySortInfo = (inputArray) => {
+  if (inputArray.some((element) => typeof element !== 'number')) {
+    return 'Некоторые элементы не являются числами'
+  }
+
+  if (
+    inputArray.every((element, index) =>
+      index > 0 ? element >= inputArray[index - 1] : true
+    )
+  ) {
+    return 'Массив отсортирован по возрастанию'
+  }
+  if (
+    inputArray.every((element, index) =>
+      index > 0 ? element <= inputArray[index - 1] : true
+    )
+  ) {
+    return 'Массив отсортирован по убыванию'
+  }
+
+  return 'Массив не отсортирован'
+}
+
 console.log(arraySortInfo(a)) // Некоторые элементы не являются числами
 console.log(arraySortInfo(b)) // Массив отсортирован по возрастанию
 console.log(arraySortInfo(c)) // Массив отсортирован по убыванию
@@ -23,20 +48,6 @@ console.log(arraySortInfo(d)) // Массив не отсортирован
 
 /**
  * ПОДСКАЗКИ
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
- *
  * Подсказка 1: Используйте метод массивов "every"
  * Подсказка 2: Вы должны использовать два параметра в колбэк функции "element", "index"
  * Подсказка 3: Каждый элемент массива, кроме первого, следует сравнивать с предыдущим
--- 43-compare-two-arrays/finish.js
+++ 43-compare-two-arrays/finish.js
@@ -11,14 +11,24 @@
  * ВАЖНО: Исходите из того, что массивы содержат элементы примитивных типов
  */
 
+const areArraysEqual = (firstArray, secondArray) => {
+  if (
+    firstArray.length === secondArray.length &&
+    firstArray.every((element, index) => element === secondArray[index])
+  ) {
+    return true
+  }
+  return false
+}
+
 const a = [1, 2, 3]
 const b = [1, 2, 3]
-
-console.log(a === b) // false (Почему?)
 
 const c = [2, 1, 3]
 const d = [1, 2, 3, 4]
 
+// Сравниваются ссылки на массивы
+console.log(a === b) // false (Почему?)
 console.log(areArraysEqual(a, b)) // true
 console.log(areArraysEqual(a, c)) // false
 console.log(areArraysEqual(a, d)) // false
