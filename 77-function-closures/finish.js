15400c62d9c0af61b16ac5fadc8d4b1afd104a5b
krawnick <kraw.nick@gmail.com>
2023-07-09 21:46:12 +0500

    tasks 77 are ready

--- 77-function-closures/finish.js
+++ 77-function-closures/finish.js
@@ -14,7 +14,26 @@
  *  - changeGreeting
  */
 
+const createGreeting = () => {
+  let greetingString = 'Hey, this is'
+
+  function greet(name) {
+    return `${greetingString} ${name}`
+  }
+
+  function changeGreeting(greet) {
+    greetingString = greet
+  }
+
+  return {
+    greet,
+    changeGreeting,
+  }
+}
+
 const greeting1 = createGreeting()
+
+console.log('greeting1', greeting1)
 
 console.log(greeting1.greet('Bob'))
 // Hey, this is Bob
